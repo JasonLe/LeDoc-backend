97d219eaebd4422f4b2bec3d28adc8f459ef6b34
JasonLe <2461251662@qq.com>
2023-04-30 16:26:36 +0800

    init project

---
+++ src/main/java/com/example/ledoc/LeDocApplication.java
@@ -0,0 +1,13 @@
+package com.example.ledoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeDocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeDocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ledoc/LeDocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ledoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeDocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
